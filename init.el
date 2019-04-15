13ca59a534a064f5cbdfe94881a78dbb25d967da
madwsa <8822326+madwsa@users.noreply.github.com>
2019-04-15 16:03:58 -0500

    inital commit

---
+++ init.el
@@ -0,0 +1,73 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; stop annoying custom-set-* showing up in this file
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; install packages locally if not installed
+
+;; package list
+(setq package-list '(zenburn-theme
+                     ess
+                     polymode
+		     poly-markdown))
+
+;; package repositories
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+;; activate all packages
+(package-initialize)
+
+;; fetch package lists
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; install missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; load & configure environment
+
+;; load theme
+(load-theme 'zenburn t)
+
+;; load ess
+(require 'ess-site)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; set look & feel
+
+(setq inhibit-startup-message t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(toggle-scroll-bar -1)
+(show-paren-mode 1)
+
+(setq linum-format "%d ")
+(global-linum-mode 1)
+(setq column-number-mode t)
+
+(setq-default indent-tabs-mode nil)
+(setq comment-style 'multi-line)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; handle backups & temporary files
+
+;; backup in one place. flat, no tree structure
+(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
+
+;; save all tempfiles in $TMPDIR/emacs$UID
+(defconst emacs-tmp-dir
+  (expand-file-name
+   (format "emacs%d" (user-uid)) temporary-file-directory))
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+      emacs-tmp-dir)
