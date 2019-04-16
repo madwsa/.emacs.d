862eecf715e53de31a6cb411ee8d578cffc8e8cf
madwsa <8822326+madwsa@users.noreply.github.com>
2019-04-15 23:46:53 -0500

    org and backup

--- init.el
+++ init.el
@@ -39,6 +39,12 @@
 ;; load ess
 (require 'ess-site)
 
+;; load org
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; set look & feel
 
